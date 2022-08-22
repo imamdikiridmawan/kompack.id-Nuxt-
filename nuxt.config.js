5ff7a3f5cf52086efddb07b87e2ed350150e8f9e
khoerul umam <khoerulumam0723@gmail.com>
2022-08-22 14:46:46 +0700

    title browser lp kompack

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Landing Page Kompack',
+    title: 'Kompack - Jasa Pergudangan di Lokasi Strategis',
     htmlAttrs: {
       lang: 'en'
     },
